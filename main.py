f03826a0ddf1fc643ea0df52f13286140b14a413
zachkaupp <zachkaupp@gmail.com>
2024-05-21 15:10:36 -0500

    get things working

---
+++ main.py
@@ -0,0 +1,21 @@
+"""main.py"""
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+scope = ["user-read-private", "user-library-read", "user-library-modify",
+         "user-read-recently-played", "user-top-read", "user-read-playback-position",
+         "playlist-modify-public", "playlist-modify-private", "playlist-read-collaborative",
+         "playlist-read-private", "streaming", "app-remote-control",
+         "user-read-currently-playing", "user-modify-playback-state", "user-read-playback-state",
+         "ugc-image-upload"]
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config["CLIENT_ID"],
+                                               client_secret=config["CLIENT_SECRET"],
+                                               redirect_uri="http://localhost:8000/callback",
+                                               scope=scope))
+
+results = sp.add_to_queue("spotify:track:4qbEaaJ29p32GI8EWQmm6R")
